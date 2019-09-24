610dc1a5fe0f5b34e11d543cd4ca8fe68dd4fd28
Syed Zaman Mostafiz <zamanwebdeveloperforssmtech@gmail.com>
2019-09-24 16:14:36 +0600

    mail function is work

--- index.php
+++ index.php
@@ -1,49 +1,28 @@
-<!DOCTYPE HTML>
+<!DOCTYPE html>
 <html>
 <head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-  <style>
-  p {
-    text-align: center;
-    font-size: 60px;
-    margin-top: 0px;
-  }
-  </style>
+	<title>Our Website</title>
 </head>
 <body>
-
-<p id="demo"></p>
-
-<script>
-// Set the date we're counting down to
-var countDownDate = new Date("Dec 31, 2019 00:00:00").getTime();
-
-// Update the count down every 1 second
-var x = setInterval(function() {
-
-  // Get today's date and time
-  var now = new Date().getTime();
-    
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
-    
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
-  // Output the result in an element with id="demo"
-  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
-    
-  // If the count down is over, write some text 
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("demo").innerHTML = "Time Up";
-  }
-}, 1000);
-</script>
+	<form action="" method="POST">
+		<input type="text" placeholder="to" name="to"><br>
+		<input type="text" placeholder="subject" name="subject"><br>
+		<input type="text" placeholder="from" name="from"><br>
+		<textarea cols="30" rows="8" name="textarea"></textarea>
+		<input type="submit" value="send">
+	</form>
 
 </body>
 </html>
+<?php
+	$to = $_POST['to'];
+	$subject = $_POST['subject'];
+	$message = $_POST['textarea'];
+	$headers = "From: ".$_POST['from']."\r\n";
+	$headers .= "Content-type: text/html;charset=UTF-8" . "\r\n";
+
+	mail($to, $subject, $message, $headers);
+
+
+
+?>
